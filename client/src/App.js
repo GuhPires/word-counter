635ca602ba7985a2667b9d3f14453d9b42612ebd
Gustavo <gustavo.goncalvespires@gmail.com>
2020-10-13 20:54:40 -0300

    Context API added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Counter from './Components/Counter';
 import Page from './Components/Page';
 import TextInput from './Components/TextInput';
+import { AppCtx } from './context';
 
 function App() {
+  const ctx = useContext(AppCtx);
   return (
-    <div className="App">
+    <div className={`App ${ctx.state.theme}`}>
       <Page name="home" title="Word Counter" theme="light">
         <TextInput />
         <Counter />
---
+++ client/src/context.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+const AppCtx = React.createContext();
+
+class AppProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: 'Light',
+      wordsObj: {},
+      words: 0,
+      chars: 0,
+      spaces: 0,
+      paragraphs: 0,
+      numbers: 0
+    }
+    this.ctxActions = {
+      theme: {
+        change: () => {
+          switch(this.state.theme) {
+            case 'Light':
+              this.setState({ theme: 'Dark' });
+              break;
+            case 'Dark':
+            default:
+              this.setState({ theme: 'Dark' });
+              break;
+          }
+        }
+      }
+    }
+  }
+  render() {
+    return <AppCtx.Provider value={{ state: this.state, ...this.ctxActions }}>
+      {this.props.children}
+    </AppCtx.Provider>
+  }
+}
+
+export { AppCtx, AppProvider }
